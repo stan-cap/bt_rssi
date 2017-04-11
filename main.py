98b057a3e6b656a2363d90e827e3664a5d714f5d
stan-cap <stanfordrpi.github.repository@gmail.com>
2017-04-11 14:50:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ BT_ADDR = 'C8:A8:23:EE:14:61'#/// Enter your bluetooth address here!
 
 # ----------------------- DO NOT EDIT ANYTHING BELOW THIS LINE --------------------------- #
 
-NUM_LOOP = 2
+NUM_LOOP = 50
 records = []
 
 
-def write(records, count):
+def write(records, count, mod_count):
     f = open("records.txt", "a+") #open records for append. If not present create
-    for i in range(count): #write out each record
+    for i in range(mod_count, count): #write out each record
         f.write(str(records[i][0]) + "," + str(records[i][1]) + '\n')
     f.close()
 
@@ -33,6 +33,7 @@ def main():
     addr = BT_ADDR
     num = NUM_LOOP
     count = 0
+    mod_count = 0
 
     while(count < num):      
         btrssi = BluetoothRSSI(addr=addr)
@@ -41,7 +42,9 @@ def main():
         records.append(record)
         count += 1
         time.sleep(.5)
-    write(records, count)
+        if(count % 5 == 0):
+            write(records, count, mod_count)
+            mod_count += 5
 
 
 if __name__ == '__main__':
