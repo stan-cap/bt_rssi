e76e6020345a9e535e1bb182fa23cf9999c647fc
stan-cap <stanfordrpi.github.repository@gmail.com>
2017-04-13 14:00:06 -0500

    Update main.py
    added comments

--- main.py
+++ main.py
@@ -15,31 +15,31 @@ BT_ADDR = 'C8:A8:23:EE:14:61'#/// Enter your bluetooth address here!
 
 
 def write(records, count):
-    f = open("records.txt", "a+") #open records for append. If not present create
-    for i in range(count): #write out each record
+    f = open("records.txt", "a+")                      # open records for append. If not present create
+    for i in range(count):                             # write out each record
         f.write(str(records[i][0]) + "," + str(records[i][1]) + '\n')
     f.close()
 
 def time_diff(start_time):
-    current_time = datetime.datetime.now()
-    diff = (current_time - start_time).total_seconds()
+    current_time = datetime.datetime.now()             # get current time
+    diff = (current_time - start_time).total_seconds() # get difference of startime and current time
     return str(diff)
 
 def main(start_time):
 
-    records = []
-    count = 0
-    addr = BT_ADDR
-    num = 10
+    records = []                                       # initialize array of records
+    count = 0                                          # initialize count
+    addr = BT_ADDR                                     # assign BT_ADDR
+    num = 10                                           # amount of records to be recorded
 
     while(count < num):      
         btrssi = BluetoothRSSI(addr=addr)
-        current_time = time_diff(start_time)
-        record = (btrssi.get_rssi(), current_time)
-        records.append(record)
+        time_e = time_diff(start_time)                 # get seconds elapsed
+        record = (btrssi.get_rssi(), time_e)           # create record
+        records.append(record)                         # add record to records array
         count += 1
-        time.sleep(.5)
-    write(records, count)
+        time.sleep(.5)                                 # wait time to get next record
+    write(records, count)                              # write out records
 
 if __name__ == '__main__':
     main()
