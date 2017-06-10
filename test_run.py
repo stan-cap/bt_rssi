3daff0720172ad50185ff98bdab33816fa39c02a
stan-cap <stanfordrpi.github.repository@gmail.com>
2017-06-10 14:23:31 -0500

    Create test_run.py
    A file modified to simplify the testing process. Use this file when testing to see if the pis are configured properly

---
+++ test_run.py
@@ -0,0 +1,15 @@
+import test_main
+import datetime
+import time
+
+f = open("test.txt", "w")            # open text file for editting
+f.write("RSSI, sec_elapsed" + '\n')  # add headers to text file
+f.close()
+
+start_time = datetime.datetime.now() # get relative start time
+exec_count = 0                       # initialize counter
+exec_amt = 2                         # amount of times to execute main.py which records 10 records
+
+while (exec_count < exec_amt):       # execute main.py while count is less than amount
+    test_main.main(start_time)
+    exec_count += 1
