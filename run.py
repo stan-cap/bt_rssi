0c752c090902d1fa85abc2d030c1066366be7f30
stan-cap <stanfordrpi.github.repository@gmail.com>
2017-06-11 15:15:26 -0500

    Create run.py
    updated sleep start time to be longer to allow clock services to set time and not effect the records

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ f = open("records.txt", "a+")        # open text file for editting
 f.write("RSSI, sec_elapsed" + '\n')  # add headers to text file
 f.close()
 
-time.sleep(10)                       # sleep for 10 seconds to allow Bluetooth Services to start
+time.sleep(45)                       # sleep for 45 seconds to allow Bluetooth Services to start
 
 start_time = datetime.datetime.now() # get relative start time
 exec_count = 0                       # initialize counter
