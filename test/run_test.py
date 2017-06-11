e90da6151954750410a3e7e322d2dccd1ba0694b
stan-cap <stanfordrpi.github.repository@gmail.com>
2017-06-11 15:16:40 -0500

    Create run_test.py
    clean comments

--- test/run_test.py
+++ test/run_test.py
@@ -10,6 +10,6 @@ start_time = datetime.datetime.now() # get relative start time
 exec_count = 0                       # initialize counter
 exec_amt = 2                         # amount of times to execute main.py which records 10 records
 
-while (exec_count < exec_amt):       # execute main.py while count is less than amount
+while (exec_count < exec_amt):       # execute main_test.py while count is less than amount
     main_test.main(start_time)
     exec_count += 1
